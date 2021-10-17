d7b624cd0dc3ebe7829fac85b4039072b18689c7
DannyAAM <danny@saru.moe>
2021-10-17 16:59:32 +0800

    potential parallel download support and some minor rewrite

--- src/downloader/downloader.cr
+++ src/downloader/downloader.cr
@@ -5,29 +5,26 @@ class Downloader
   @resume_file : File?
 
   def initialize(@auto_merge : Bool? = nil, @merge = false, @no_resume = false, @auth_token = "", @cookies = "")
-    @clients = Hash(Tuple(Symbol, String), HTTP::Client).new
+    @clients = Hash(Tuple(Symbol, String, Int32), HTTP::Client).new do |h, k|
+      type, host, num = k
+      h[k] = HTTP::Client.new host, tls: true
+    end
     @resuming = false
   end
 
-  def client(uri : URI, type = :main)
-    host = uri.host.not_nil!
-    key = {type, host}
-    client = @clients[key]?
-    if client.nil?
-      client = HTTP::Client.new host, tls: true
-      @clients[key] = client
-    end
-    client.not_nil!
+  def client(uri : URI, type = :main, num = 0)
+    @clients[{type, uri.host.not_nil!, num}]
   end
 
-  def exec(method : String, uri : URI, type = :main, headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType = nil, depth = 0, &block : HTTP::Client::Response -> )
+  def exec(method : String, uri : URI, type = :main, path = "", headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType = nil, depth = 0, &block : HTTP::Client::Response -> )
     depth += 1
     raise "too many redirection" if depth > 15
+    uri = uri.resolve path unless path.empty?
     client(uri, type).exec method, uri.request_target, headers, body do |res|
       case res.status_code
       when 301, 302
         puts "Redirection"
-        exec method, URI.parse(res.headers["Location"]), type, headers, body, depth, &block
+        exec method, URI.parse(res.headers["Location"]), type, path, headers, body, depth, &block
       else
         yield res
       end
@@ -36,17 +33,17 @@ class Downloader
 
   DUMMY_RES = HTTP::Client::Response.new 404
   {% for method in %w(get post) %}
-    def {{method.id}}(uri : URI, type = :main, headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType = nil)
+    def {{method.id}}(uri : URI, type = :main, path = "", headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType = nil)
       ret_res = DUMMY_RES
-      exec {{method.upcase}}, uri, type, headers, body do |res|
+      exec {{method.upcase}}, uri, type, path, headers, body do |res|
         res.consume_body_io
         ret_res = res
       end
       ret_res
     end
 
-    def {{method.id}}(uri : URI, type = :main, headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType = nil, &block : HTTP::Client::Response -> )
-      exec {{method.upcase}}, uri, type, headers, body, &block
+    def {{method.id}}(uri : URI, type = :main, path = "", headers : HTTP::Headers? = nil, body : HTTP::Client::BodyType = nil, &block : HTTP::Client::Response -> )
+      exec {{method.upcase}}, uri, type, path, headers, body, &block
     end
   {% end %}
 
--- src/downloader/yt_downloader.cr
+++ src/downloader/yt_downloader.cr
@@ -14,6 +14,8 @@ class YTDownloader < Downloader
 
   YT = URI.parse "https://www.youtube.com"
 
+  TYPES = %w{video audio}
+
   @video_file : File?
   @audio_file : File?
 
@@ -36,144 +38,12 @@ class YTDownloader < Downloader
     @streaming = true
   end
 
-  macro gen_video_audio
-    {% types = %w{video audio} %}
-
-    {% for type in types %}
-      def set_{{type.id}}(@{{type.id}}_url, @{{type.id}}_size = -1_i64)
-        @{{type.id}}_uri = URI.parse @{{type.id}}_url
-        @{{type.id}}_query = @{{type.id}}_uri.query_params
-      end
-    {% end %}
-
-    def parse_video_web
-      res = client(YT).get "/watch?v=#{@id}"
-      if res.status_code != 200
-        raise "Unable to retrieve chat basic info, HTTP #{res.status_code}"
-        return
-      end
-      {% for type in types %}
-        {{type.id}}_url = ""
-        {{type.id}}_bitrate = 0
-        {{type.id}}_size = -1_i64
-      {% end %}
-      html = Lexbor::Parser.new res.body
-      @date = Time.local.to_s("%Y%m%d") # fallback
-      html.css("script:not([src])").each do |node|
-        case node.inner_text
-        when /var ytInitialPlayerResponse\s*=\s*({.*?});/
-          json = JSON.parse $~[1]
-          video_details = json["videoDetails"]
-          @title = video_details["title"].as_s
-          microformat = json["microformat"]["playerMicroformatRenderer"]
-          if video_details["isLive"]?.try &.as_bool
-            @date = Time.parse_rfc3339(microformat["liveBroadcastDetails"]["startTimestamp"].as_s).to_s("%Y%m%d")
-          else
-            @date = microformat["uploadDate"].as_s.gsub("-") {}
-          end
-          formats = json["streamingData"]["adaptiveFormats"].as_a
-          formats.each do |format|
-            case format["mimeType"].as_s
-            {% for type in types %}
-              when .starts_with? "{{type.id}}"
-                bitrate = format["bitrate"].as_i
-                if bitrate > {{type.id}}_bitrate
-                  url = extract_url_from_format format
-                  unless url.empty?
-                    size = format["contentLength"]?.try &.as_s
-                    unless size.nil?
-                      @streaming = false
-                      {{type.id}}_size = size.not_nil!.to_i64
-                    end
-                    {{type.id}}_bitrate = bitrate
-                    {{type.id}}_url = url
-                  end
-                end
-            {% end %}
-            end
-          end
-        end
-      end
-      {% for type in types %}
-        raise NotSupportedError.new if {{type.id}}_url.empty?
-        set_{{type.id}} {{type.id}}_url, {{type.id}}_size
-      {% end %}
+  {% for type in TYPES %}
+    def set_{{type.id}}(@{{type.id}}_url, @{{type.id}}_size = -1_i64)
+      @{{type.id}}_uri = URI.parse @{{type.id}}_url
+      @{{type.id}}_query = @{{type.id}}_uri.query_params
     end
-
-    def dl_segment
-      case @type
-      {% for type in types %}
-        when :{{type.id}}
-          file = @{{type.id}}_file.not_nil!
-          @{{type.id}}_query["sq"] = @sq.to_s
-          @{{type.id}}_uri.query_params = @{{type.id}}_query
-          get @{{type.id}}_uri do |res|
-            fail = true
-            puts "Getting {{type.id}}-#{@sq}.ts"
-            case res.status_code
-            when 200
-              prev_pos = file.pos
-              begin
-                IO.copy res.body_io, file
-                file.flush
-                puts "{{type.id}}-#{@sq}.ts done"
-                step
-                fail = false
-              rescue ex : IO::Error
-                file.pos = prev_pos
-              end
-            else
-              res.body_io?.try &.gets_to_end # consume remaining body
-            end
-
-            if fail
-              puts "Retrieving {{type.id}}-#{@sq}.ts failed, retry #{@fail}/30"
-              sleep 1.second
-              @fail += 1
-              if @fail > 30
-                raise "failed - maybe normal if last file downloaded"
-              end
-            end
-          end
-      {% end %}
-      end
-    end
-
-    def dl_single
-      channel = Channel(Nil).new
-      {% for type in types %}
-        spawn do
-          file = @{{type.id}}_file.not_nil!
-          if @resuming
-            file.seek(0, IO::Seek::End)
-            @{{type.id}}_query["range"] = "#{file.pos}-"
-            @{{type.id}}_uri.query_params = @{{type.id}}_query
-          end
-          total = @{{type.id}}_size
-          last = current = start = file.pos
-          puts "Retrieving {{type.id}}, #{current.humanize_bytes} of #{total.humanize_bytes}"
-          get @{{type.id}}_uri, :{{type.id}} do |res|
-            io = res.body_io
-            buffer = uninitialized UInt8[4096]
-            time = Time.monotonic
-            while (len = io.read(buffer.to_slice).to_i32) > 0
-              file.write buffer.to_slice[0, len]
-              current &+= len
-              if current - last > 1048576_i64
-                last = current
-                speed = ((current - start) / (Time.monotonic - time).total_seconds).round.to_i64
-                puts "Retrieving {{type.id}}, #{current.humanize_bytes} of #{total.humanize_bytes}, #{speed.humanize_bytes}/s"
-              end
-              Fiber.yield
-            end
-            channel.send nil
-          end
-        end
-      {% end %}
-      {{types.size}}.times { channel.receive }
-    end
-  end
-  gen_video_audio
+  {% end %}
 
   def load_resume_file
     @resume_file = File.open(tmp_name(:resume), "r+")
@@ -271,6 +141,103 @@ class YTDownloader < Downloader
     @resume_file.try &.delete
   end
 
+  def parse_video_web
+    res = get YT, path: "/watch?v=#{@id}"
+    if res.status_code != 200
+      raise "Unable to retrieve chat basic info, HTTP #{res.status_code}"
+      return
+    end
+    {% for type in TYPES %}
+      {{type.id}}_url = ""
+      {{type.id}}_bitrate = 0
+      {{type.id}}_size = -1_i64
+    {% end %}
+    html = Lexbor::Parser.new res.body
+    @date = Time.local.to_s("%Y%m%d") # fallback
+    html.css("script:not([src])").each do |node|
+      {% begin %}
+        case node.inner_text
+        when /var ytInitialPlayerResponse\s*=\s*({.*?});/
+          json = JSON.parse $~[1]
+          video_details = json["videoDetails"]
+          @title = video_details["title"].as_s
+          microformat = json["microformat"]["playerMicroformatRenderer"]
+          if video_details["isLive"]?.try &.as_bool
+            @date = Time.parse_rfc3339(microformat["liveBroadcastDetails"]["startTimestamp"].as_s).to_s("%Y%m%d")
+          else
+            @date = microformat["uploadDate"].as_s.gsub("-") {}
+          end
+          formats = json["streamingData"]["adaptiveFormats"].as_a
+          formats.each do |format|
+            case format["mimeType"].as_s
+            {% for type in TYPES %}
+              when .starts_with? "{{type.id}}"
+                bitrate = format["bitrate"].as_i
+                if bitrate > {{type.id}}_bitrate
+                  url = extract_url_from_format format
+                  unless url.empty?
+                    size = format["contentLength"]?.try &.as_s
+                    unless size.nil?
+                      @streaming = false
+                      {{type.id}}_size = size.not_nil!.to_i64
+                    end
+                    {{type.id}}_bitrate = bitrate
+                    {{type.id}}_url = url
+                  end
+                end
+            {% end %}
+            end
+          end
+        end
+      {% end %}
+    end
+    {% for type in TYPES %}
+      raise NotSupportedError.new if {{type.id}}_url.empty?
+      set_{{type.id}} {{type.id}}_url, {{type.id}}_size
+    {% end %}
+  end
+
+  def dl_segment
+    {% begin %}
+      case @type
+      {% for type in TYPES %}
+        when :{{type.id}}
+          file = @{{type.id}}_file.not_nil!
+          @{{type.id}}_query["sq"] = @sq.to_s
+          @{{type.id}}_uri.query_params = @{{type.id}}_query
+          get @{{type.id}}_uri do |res|
+            fail = true
+            puts "Getting {{type.id}}-#{@sq}.ts"
+            case res.status_code
+            when 200
+              prev_pos = file.pos
+              begin
+                IO.copy res.body_io, file
+                file.flush
+                puts "{{type.id}}-#{@sq}.ts done"
+                step
+                fail = false
+              rescue ex : IO::Error
+                file.pos = prev_pos
+              end
+            else
+              res.body_io?.try &.skip_to_end # consume remaining body
+            end
+
+            if fail
+              puts "Retrieving {{type.id}}-#{@sq}.ts failed, retry #{@fail}/30"
+              sleep 1.second
+              @fail += 1
+              if @fail > 30
+                raise "failed - maybe normal if last file downloaded"
+              end
+            end
+          end
+      {% end %}
+      end
+    {% end %}
+  end
+
   def step
     @fail = 0
     case @type
@@ -281,6 +248,40 @@ class YTDownloader < Downloader
       @sq += 1
       update_resume_file
     end
+  end
+
+  def dl_single
+    channel = Channel(Nil).new
+    {% for type in TYPES %}
+      spawn do
+        file = @{{type.id}}_file.not_nil!
+        if @resuming
+          file.seek(0, IO::Seek::End)
+          @{{type.id}}_query["range"] = "#{file.pos}-"
+          @{{type.id}}_uri.query_params = @{{type.id}}_query
+        end
+        total = @{{type.id}}_size
+        last = current = start = file.pos
+        puts "Retrieving {{type.id}}, #{current.humanize_bytes} of #{total.humanize_bytes}"
+        get @{{type.id}}_uri, :{{type.id}} do |res|
+          io = res.body_io
+          buffer = uninitialized UInt8[4096]
+          time = Time.monotonic
+          while (len = io.read(buffer.to_slice).to_i32) > 0
+            file.write buffer.to_slice[0, len]
+            current &+= len
+            if current - last > 1048576_i64
+              last = current
+              speed = ((current - start) / (Time.monotonic - time).total_seconds).round.to_i64
+              puts "Retrieving {{type.id}}, #{current.humanize_bytes} of #{total.humanize_bytes}, #{speed.humanize_bytes}/s"
+            end
+            Fiber.yield
+          end
+          channel.send nil
+        end
+      end
+    {% end %}
+    {{TYPES.size}}.times { channel.receive }
   end
 
   def filename
